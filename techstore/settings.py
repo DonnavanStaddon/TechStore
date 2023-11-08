c5e601cc6843068d6354a0637b7c2d0ef8921462
Donnavan Stadd <donnystaddon@gmail.com>
2023-11-08 14:56:44 +0000

    Added product id and products detail

--- techstore/settings.py
+++ techstore/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#ra*90&+7ae_=t7(i%tog+lg1*!g%zu=1uznh%rbj^y(a4&mg%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-donnavanstadd-techstore-w40pg95m7ab.ws-eu106.gitpod.io']
+ALLOWED_HOSTS = ['8000-donnavanstadd-techstore-9d0k3lfzeej.ws-eu106.gitpod.io']
 
 
 # Application definition
