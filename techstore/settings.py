7abff29c978dc8356ebe32f97dc852e40d11ebc5
Donnavan Stadd <donnystaddon@gmail.com>
2023-11-13 20:40:52 +0000

    Added add to bag functionality

--- techstore/settings.py
+++ techstore/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#ra*90&+7ae_=t7(i%tog+lg1*!g%zu=1uznh%rbj^y(a4&mg%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-donnavanstadd-techstore-73ruxfj2g27.ws-eu106.gitpod.io']
+ALLOWED_HOSTS = ['8000-donnavanstadd-techstore-312blc8hjnr.ws-eu106.gitpod.io']
 
 
 # Application definition
