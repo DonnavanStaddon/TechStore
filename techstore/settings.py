15be307b8b9281f5b35f1c99dcc4c208aec7cd84
Donnavan Stadd <donnystaddon@gmail.com>
2023-11-17 08:21:26 +0000

    Added quantity input plus and minus buttons

--- techstore/settings.py
+++ techstore/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#ra*90&+7ae_=t7(i%tog+lg1*!g%zu=1uznh%rbj^y(a4&mg%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-donnavanstadd-techstore-8z1j10micty.ws-eu106.gitpod.io']
+ALLOWED_HOSTS = ['8000-donnavanstadd-techstore-bf7gjnfgmid.ws-eu106.gitpod.io']
 
 
 # Application definition
