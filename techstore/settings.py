7f971fab360405e236e9eb588d13007a4a464aa6
Donnavan Stadd <donnystaddon@gmail.com>
2023-11-22 09:06:47 +0000

    Fixed javascript pop up

--- techstore/settings.py
+++ techstore/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#ra*90&+7ae_=t7(i%tog+lg1*!g%zu=1uznh%rbj^y(a4&mg%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-donnavanstadd-techstore-g9ljaxjgug4.ws-eu106.gitpod.io']
+ALLOWED_HOSTS = ['8000-donnavanstadd-techstore-yk7rverps39.ws-eu106.gitpod.io']
 
 
 # Application definition
