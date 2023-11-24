89c861d69977992abaf966a9a09143befe4be0dc
Donnavan Stadd <donnystaddon@gmail.com>
2023-11-24 19:23:27 +0000

    Admin signals p1

--- techstore/settings.py
+++ techstore/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#ra*90&+7ae_=t7(i%tog+lg1*!g%zu=1uznh%rbj^y(a4&mg%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-donnavanstadd-techstore-cezjqzq17jk.ws-eu106.gitpod.io']
+ALLOWED_HOSTS = ['8000-donnavanstadd-techstore-71d7gd56z5z.ws-eu106.gitpod.io']
 
 
 # Application definition
