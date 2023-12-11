54a3456e3696cb591f7bf5d86ed80130c9d04fd0
Donnavan Stadd <donnystaddon@gmail.com>
2023-12-11 08:35:46 +0000

    Update of Allauth

--- techstore/settings.py
+++ techstore/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#ra*90&+7ae_=t7(i%tog+lg1*!g%zu=1uznh%rbj^y(a4&mg%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-donnavanstadd-techstore-51zoczvqmsu.ws-eu106.gitpod.io']
+ALLOWED_HOSTS = ['8000-donnavanstadd-techstore-o0nuz3d8qgs.ws-eu106.gitpod.io']
 
 
 # Application definition
